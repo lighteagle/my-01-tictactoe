4a76a621d5a1b66d2f8b60d14cce2c4f52600752
lighteagle <sofyan.agus18@gmail.com>
2020-01-01 15:01:47 +0700

    update tictactoe basic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Game from "./components/Game";
+
+function App() {
+  return (
+    <div>
+      <h1> My Tic Tac Toe</h1>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
